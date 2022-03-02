e2d4786df337a786fcf4787a1a951e96303c6d51
harmonbhasin <62266474+harmonbhasin@users.noreply.github.com>
2022-03-02 11:00:26 -0600

    Added my name and michaels to the about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -7,12 +7,17 @@ import {
   Heading,
   Flex,
   Container,
+  HStack,
 } from '@chakra-ui/react'
 
 const About: NextPage = () => (
   <Box>
     <Container maxW={'4xl'} centerContent>
       <Heading size={'4xl'}>About</Heading>
+      <HStack>
+        <Heading>Harmon</Heading>
+        <Heading>Michael</Heading>
+      </HStack>
       <Box height={'1000px'}></Box>
     </Container>
   </Box>
