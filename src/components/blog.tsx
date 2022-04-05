ab8c70c3391acb3716166eb68e72b857c25ba5e7
harmonbhasin <62266474+harmonbhasin@users.noreply.github.com>
2022-04-05 17:40:14 -0500

    Fixed code blog

--- src/components/blog.tsx
+++ src/components/blog.tsx
@@ -239,23 +239,4 @@ const ArticleList = () => {
   )
 }
 
-export async function getStaticProps() {
-  const files = fs.readdirSync('uploads')
-
-  let articles = files.map((file) => {
-    const data = fs.readFileSync(`uploads/${file}`).toString()
-
-    return {
-      ...matter(data).data,
-      slug: file.split('.')[0],
-    }
-  })
-
-  return {
-    props: {
-      articles: articles,
-    },
-  }
-}
-
 export default ArticleList
